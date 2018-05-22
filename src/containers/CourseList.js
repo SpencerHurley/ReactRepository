bc59c5e0b082901d8089bb42677fcb328eb48ffb
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2018-05-21 21:08:37 -0400

    CourseList updates

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,1 +1,35 @@
 import React, { Component } from 'react';
+
+class CourseList extends Component {
+
+
+    renderListCourses(courses) {
+        let courseList = this.state.courses.map(function(course) {
+            return <CourseRow title={course.title}
+                              ownedBy={course.ownedBy}
+                              lastUpdated={course.lastUpdated}
+            />
+        });
+        return courseList;
+    }
+}
+
+class CourseRow extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <tr className="list-group-item">
+                <td>{this.props.title}</td>
+                <td>{this.props.ownedBy}</td>
+                <td>{this.props.lastUpdated}</td>
+                <td>
+                    <button onClick={this.delete.bind(this)}>Delete</button>
+                </td>
+            </tr>
+        )
+    }
+
+}
