099c0b524121bc13b2d6529e6c1f44062a82403c
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2018-05-24 13:29:08 -0400

    Updates to ModuleList

--- src/containers/ModuleList.js
+++ src/containers/ModuleList.js
@@ -5,16 +5,36 @@ class ModuleList extends React.Component {
     constructor(props) {
         super(props);
         this.moduleService = ModuleServiceClient.instance;
+        this.setState({
+            modules: []
+        })
+    }
+
+    componentDidMount() {
+        this.moduleService.findAllModules().then((value) => this.setState({modules : value}));
+    }
+
+    render() {
+        return this.renderModules(listModules);
     }
 
     renderModules(listModules){
 
     }
 
-    render() {
-        let listModules = moduleService.findAllModules();
-        return renderModules(listModules);
+}
+
+class ModuleRow extends React.Component {
+    constructor(props) {
+        super(props);
     }
 
+    render() {
+        return (
+            <li className="list-group-item">
+                    <i>{this.props.module.title}</i>
+            </li>
+        )
+    }
 }
 
