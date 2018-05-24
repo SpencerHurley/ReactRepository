6e59d958b0a17d69a4c4ab6626e7b80f4231e6d0
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2018-05-24 00:49:33 -0400

    Editing CourseManager

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -8,13 +8,43 @@ class CourseList extends Component {
         this.courseService = CourseServiceClient.instance;
         this.createCourse = this.createCourse.bind(this);
         this.deleteCourse = this.deleteCourse.bind(this);
+        this.titleChanged = this.titleChanged.bind(this);
         this.state = {
-            courses: []
+            courses: [],
+            course: {
+                title: '',
+                id: 0
+            }
         }
     }
 
     render() {
-        return this.renderListCourses(this.state.courses);
+        return (
+            <div>
+                <div class="row">
+                    Course Manager
+                    <input className="form-control"
+                           placeholder="New Course Title"
+                           value={this.state.course.title}
+                           onChange={this.titleChanged}
+                    />
+                    <button
+                        className="btn btn-primary btn-block"
+                        onClick={this.createCourse}
+                    >
+                        <i className="fa fa-plus"></i>
+                    </button>
+                </div>
+                <ul className="list-group">
+                    <span className="float-left">
+                        <i>Title </i>
+                        <i> Owned By  </i>
+                        <i>Last Updated</i>
+                    </span>
+                    {this.renderListCourses(this.state.courses)}
+                </ul>
+            </div>
+        )
     }
 
     componentDidMount() {
@@ -23,20 +53,30 @@ class CourseList extends Component {
 
     renderListCourses(courses) {
         let courseList = courses.map(function(course) {
-            return <CourseRow title={course.title}
-                              ownedBy={course.modified}
-                              lastUpdated={course.modified}
-            />
+            return <CourseRow course={course}/>
         });
         return courseList;
     }
 
-    createCourse() {
-
+    titleChanged(e) {
+        this.setState({
+            course: {title: e.target.value}
+        });
     }
 
-    deleteCourse() {
+    createCourse() {
+        this.state.course.id = this.state.courses.length + 1;
+        console.log(this.state.course);
+        this.courseService.createCourse(this.state.course);
+        this.setState({
+            course : {
+                title: ''
+            }
+        })
+    }
 
+    deleteCourse(id) {
+        this.courseService.deleteCourse(id);
     }
 
     
@@ -49,12 +89,17 @@ class CourseRow extends Component {
 
     render() {
         return (
-            <tr className="list-group-item">
-                <td>{this.props.title}</td>
-                <td>{this.props.ownedBy}</td>
-                <td>{this.props.modified}</td>
-
-            </tr>
+            <li className="list-group-item">
+                <span className="float-left">
+                    <i>{this.props.course.title}</i>
+                    <i> me  </i>
+                    <i>{this.props.course.modified}</i>
+                </span>
+                <span className="float-right">
+                 <i className="fa fa-trash"></i>
+                 <i className="fa fa-pencil"></i>
+               </span>
+            </li>
         )
     }
 }
--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -6,19 +6,7 @@ export default class CourseManager extends Component {
     render() {
         return (
             <div>
-                <tr style={{backgroundColor : '#72a9ff', }}>
-                    <td style = {{}}>Course Manager</td>
-                    <td></td>
-                    <td>
-                        <form>
-                            <label>
-                                Name:
-                                <input type="text" name="name" />
-                            </label>
-                            <input type="submit" value="Submit" />
-                        </form>
-                    </td>
-                </tr>
+                <h1></h1>
                 <CourseList/>
             </div>
             )
